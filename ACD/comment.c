578cc198d48ee7e82177b9dfe6c413a022cd7bf9
Ranganath <ranganath.pai@gmail.com>
2016-08-09 15:24:38 +0530

    ACD

---
+++ ACD/comment.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+void print_state(int state)
+{
+  switch(state)
+  {
+    case 0: printf("OTHER");
+            break;
+    case 1: printf("COMMENT_START_START");
+            break;
+    case 2: printf("COMMENT_START");
+            break;
+    case 3: printf("COMMENT_END_START");
+            break;
+    case 4: printf("COMMENT_END_END");
+            break;
+
+
+  }
+}
+void main()
+{
+  enum states{ OTHER, COMMENT_START_START, COMMENT_START, COMMENT_END_START, COMMENT_END_END};
+  int current_state = OTHER;
+  int i=0;
+  char line[30];
+  scanf("%s", line);
+  for(i=0; i<strlen(line);i++)
+  {
+    if(current_state == OTHER)
+    {
+      if(line[i] == '/')
+        current_state = COMMENT_START_START;
+      continue;
+    }
+    if(current_state == COMMENT_START_START)
+    {
+      if(line[i] == '/')
+      {
+          current_state = COMMENT_END_END;
+          break;
+      }
+      else if(line[i] == '*')
+        current_state = COMMENT_START;
+      else
+        current_state = OTHER;
+      continue;
+    }
+    if(current_state == COMMENT_START)
+    {
+      if(line[i] == '*')
+        current_state = COMMENT_END_START;
+      continue;
+    }
+    if(current_state == COMMENT_END_START)
+    {
+      if(line[i] == '/')
+        current_state = COMMENT_END_END;
+      else
+        current_state = COMMENT_START;
+      continue;
+    }
+    if (current_state == COMMENT_END_END)
+      current_state = OTHER;
+  }
+  print_state(current_state);
+  if(current_state != COMMENT_END_END)
+    printf("\nLine is not a comment ");
+}
